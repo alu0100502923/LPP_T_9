4bf97f07f829771b8b73de3e68584e807f6d61d9
Joel Perez Ramos <alu0100723304@ull.edu.es>
2014-10-17 17:00:30 +0100

    Subiendo Codigo

---
+++ test/tc_complejo.rb
@@ -0,0 +1,35 @@
+require 'lib/complejo'
+require 'test/unit'
+
+class TestComplejo < Test::Unit::TestCase
+   attr_reader :p, :q, :r, :s, :t, :u, :n
+    def setup 
+ 	@p=Complejo.new(4.0,2.0)
+	@n=Complejo.new(5.0,4.0)
+        @q=p+n
+	@r=p-n
+	@s=p*n
+	@t=p/n
+	@u=p*(3)
+    end
+
+    def test_suma
+       assert_equal(q.a,9.0)
+    end
+
+    def test_resta
+	assert_equal(r.a,-1.0)	  
+    end
+
+    def test_producto_escalar
+        assert_equal(u.a,12.0)
+    end
+
+    def test_division
+	assert_equal("0.6829268292682927",t.a.to_s)	
+    end
+    
+    def test_multiplicacion
+	assert_equal(s.a,12.0)
+    end
+end
